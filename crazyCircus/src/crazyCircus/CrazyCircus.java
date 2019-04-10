cd59898d7519d36fcc9f7aec3e2bf93183fcf56e
mikal ziane <mikal.ziane@lip6.fr>
2019-04-10 15:15:45 +0200

    initial commit

---
+++ crazyCircus/src/crazyCircus/CrazyCircus.java
@@ -0,0 +1,5 @@
+package crazyCircus;
+
+public class CrazyCircus {
+	
+}
---
+++ crazyCircus/src/crazyCircus/CrazyCircusTest.java
@@ -0,0 +1,14 @@
+package crazyCircus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CrazyCircusTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
